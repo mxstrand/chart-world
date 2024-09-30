395be70672912feba62d3b6bc569e65a736b71b8
Mike Strand <mxstrand@hotmail.com>
2024-09-30 23:27:07 +0000

    feat: Pagecontroller handles nav items

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,16 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends BaseController
+class HomeController extends PageController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
         $navItems = $this->getNavItems();
 
-        return $this->render('home/index.html.twig', [
+        return $this->renderWithNavItems('home/index.html.twig', [
             'message' => 'Welcome to your homepage!',
-            'navItems' => $navItems,
         ]);
     }
 }
